277c7aebeca4c4ace52d09629cdc62d1c7c6431c
Vagner Bellacosa <vagnerbellacosa@Gmail.com>
2021-07-06 19:31:36 -0300

    Forms testes e exemplos

---
+++ HTML_Formularios/FormValidation/js/FormEmail.js
@@ -0,0 +1,9 @@
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an e-mail address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
